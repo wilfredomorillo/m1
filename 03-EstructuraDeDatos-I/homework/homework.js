d9bb8de080e9cf69b209a7565c2da4bed079b3d5
wilfredomorillo <wilfredo.morillo@hotmail.com>
2022-05-16 14:09:45 +0200

    otro mmas

--- 03-EstructuraDeDatos-I/homework/homework.js
+++ 03-EstructuraDeDatos-I/homework/homework.js
@@ -7,9 +7,9 @@ function nFactorial(n) {
   // devolvé el factorial de n (n!)
   // ej:
   // el factorial de 3 es 6 (3 * 2 * 1)
-  if(n===1) return 1
-  if (n===0) return 0
-  return n*nFactorial (n-1)
+  if (n===  1 || n ===0) { return 1}
+  
+  return n * nFactorial (n-1)
 }
 
 function nFibonacci(n) {
@@ -18,32 +18,36 @@ function nFibonacci(n) {
   // nFibonacci(0) // 0  // el elemento 0 es cero
   // nFibonacci(1) // 1 // el elemento 1 es 1
   // nFibonacci(6) // 1 // el elemento 6 es 8
-
-if (n=== 1)return 1
-if (n===0) return 0
-
-  return nFibonacci(n-1) + nFibonacci(n-2)
+if ( n===0) return 0
+if (n=== 1 ) return 1
+ return nFibonacci(n-1) + nFibonacci (n-2)
+ //secuencia fibonacci se puede resilver iterativamente osea 
+ //con un for forech wile y cualquier otro metodo para para hacer un contador 
+ // decimos que la secuenca fibonacci se resulve con recurcion ya q con esta podemos llamar nuevamente a la funcion 
+ //y de esta forma poder tomar los elementos de dicha funcion y utilizarlos para resolver esto de una forma mas sensilla e intuitibamente 
 
 
 }
-
+function Queue(){
 // Para esta parte no es necesario utilizar recursión.
 // Implementa la clase Queue que debe contener los siguientes métodos:
 // enqueue: Agrega un valor a la queue. Respeta el orden existente.
 // dequeue: Remueve un valor de la queue. Obedece a FIFO y respeta el underflow (devuelve undefined cuando la queue tiene size cero, o sea, cuando no tiene ningún elemento).
 // size: Devuelve el número de elementos que contiene la queue.
+this.queue= []
 
-function Queue() {
-  this.queso= []
+Queue.prototype.enqueue= function(elemento){
+this.queue.push(elemento)//quiero agregar elemento a la cola 
+}
+Queue.prototype.dequeue= function(){
+return this.queue.shift()// shift eliminar primer elemento de la cola 
 
+}
 
-  Queue.prototype.enqueue() = funcion(data)
-      this.queso.push(data)
-  
+Queue.prototype.size= function(){
+  return this.queue.length // contabilizar cuantos elementos tiene la cola 
 
-  Queue.prototype.dequeue() = funcion()
-
-  Queue.prototype.size() = funcion()
+}
 
 
 }
